02d04520812f2b7fa5fcde0c3fc93883900e7a3e
Thomas Kint <thomas.kint@capgemini.com>
2018-11-28 14:03:10 +0100

    Fix maven compiler version

--- medictionary-back/medictionary-back-appli/src/main/java/fr/epsi/i5/medictionary/back/appli/decisionTree/Decision.java
+++ medictionary-back/medictionary-back-appli/src/main/java/fr/epsi/i5/medictionary/back/appli/decisionTree/Decision.java
@@ -7,50 +7,57 @@ package fr.epsi.i5.medictionary.back.appli.decisionTree;
 
 import com.lemilliard.decisiontree.Config;
 import com.lemilliard.decisiontree.DecisionTree;
+
 import java.util.List;
+import java.util.Stack;
 
 /**
- *
  * @author kbouzan
  */
 public class Decision {
-    
-    private static Config config;
-    
-    private static void setConfig(){
-        config = new Config("./exemples/exemple_medic");
 
-        config.addAttribut("fievre", "non", "oui");
-        config.addAttribut("maux de tête", "non", "oui");
-        config.addAttribut("douleur", "non", "oui");
-        config.addAttribut("insomnie", "non", "oui");
-        config.addAttribut("toux", "non", "oui");
-        config.addAttribut("trouble cardiaque", "non", "oui");
+	private static Config config;
 
-        config.addDecision("Aspirine");
-        config.addDecision("Clopidogrel");
-        config.addDecision("Ibuprofene");
-        config.addDecision("Diclofenac");
-        config.addDecision("Fenspiride");
-        config.addDecision("Hydroxyzine");
-        config.addDecision("Dextromethorphane");
-        config.addDecision("Pholcodine");
-        config.addDecision("T'es con ! T'as rien !");
-    }
-    
-    private static void generateTree(List<Integer> listAllergies){
-        Integer[] arrayAllergies = new Integer[listAllergies.size()];
-        
-        arrayAllergies = listAllergies.toArray(arrayAllergies);
-                
-        DecisionTree decisionTree = new DecisionTree(config, arrayAllergies);
+	private static void setConfig() {
+		config = new Config("./exemples/exemple_medic");
 
-        decisionTree.getTree().generateTree(config.getAttributIndexes());
+		config.addAttribut("fievre", "non", "oui");
+		config.addAttribut("maux de tête", "non", "oui");
+		config.addAttribut("douleur", "non", "oui");
+		config.addAttribut("insomnie", "non", "oui");
+		config.addAttribut("toux", "non", "oui");
+		config.addAttribut("trouble cardiaque", "non", "oui");
 
-        decisionTree.print();
-    }    
-    
-    public static void decide() {
-        
-    }
+		config.addDecision("Aspirine");
+		config.addDecision("Clopidogrel");
+		config.addDecision("Ibuprofene");
+		config.addDecision("Diclofenac");
+		config.addDecision("Fenspiride");
+		config.addDecision("Hydroxyzine");
+		config.addDecision("Dextromethorphane");
+		config.addDecision("Pholcodine");
+		config.addDecision("T'es con ! T'as rien !");
+	}
+
+	private static void generateTree(List<Integer> listAllergies) {
+		Integer[] arrayAllergies = new Integer[listAllergies.size()];
+
+		arrayAllergies = listAllergies.toArray(arrayAllergies);
+
+		DecisionTree decisionTree = new DecisionTree(config, arrayAllergies);
+
+		decisionTree.getTree().generateTree(config.getAttributIndexes());
+
+		decisionTree.print();
+	}
+
+	public static void decide() {
+
+	}
+
+	private static Stack<String> getCompatibles(List<String> molecules) {
+		Stack<String> compatibles = new Stack<>();
+
+		return compatibles;
+	}
 }
