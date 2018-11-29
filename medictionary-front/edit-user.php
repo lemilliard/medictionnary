637ff71fc825c7bd4d6e141690fbc52ee14920df
Thomas Kint <thomas.kint@capgemini.com>
2018-11-29 11:55:47 +0100

    Update front

---
+++ medictionary-front/edit-user.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<?php require ('login-modal.php'); ?>
+<?php require ('register-modal.php'); ?>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta http-equiv="x-ua-compatible" content="ie=edge">
+  <title>Auto-diagnostic & Précommande en ligne avec Medictionnary</title>
+  <!-- Font Awesome -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <!-- Bootstrap core CSS -->
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <!-- Material Design Bootstrap -->
+  <link href="css/mdb.min.css" rel="stylesheet">
+  <!-- Your custom styles (optional) -->
+   <!-- Core Style CSS -->
+    <link rel="stylesheet" href="css-diag/styles.css">
+  <link href="css/style.min.css" rel="stylesheet">
+  <link href="css/style.css" rel="stylesheet">
+  <style type="text/css">
+    html,
+    body,
+    header,
+    .view {
+      height: 100%;
+    }
+	
+	
+	header {
+		height: 100px;
+	}
+
+    @media (max-width: 740px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 1050px;
+      }
+    }
+
+    @media (min-width: 800px) and (max-width: 850px) {
+      html,
+      body,
+      header,
+      .view {
+        height: 700px;
+      }
+    }
+
+    @media (min-width: 800px) and (max-width: 850px) {
+              .navbar:not(.top-nav-collapse) {
+                  background: #1C2331!important;
+              }
+          }
+
+
+  </style>
+</head>
+
+<body>
+
+  <?php require('header.php'); ?>
+
+  <!--Main layout-->
+  <main>
+    <div id="autodiagnostic" class="container">
+
+      <!--Section: Main info-->
+      <section class="mt-5">
+
+        <!--Grid row-->
+        <div class="row justify-content-center text-center">
+			<div class="col-12 col-lg-12 ">
+					<?php require ('user-menu.php'); ?>
+                </div>
+          <!--Grid column-->
+          <div class="col-md-8 mb-4 ">
+
+            
+						 <h2 class="h3 mt-4 mb-3">Modifier profil</h2>
+						 <form class="md-form">
+						<?php
+						echo 'user session : '. $_SESSION['idUser'] .' $$$$'; 
+						echo 'user id : '. $idUser .' $$$$'; 
+						echo 'user is login : '. $is_login .' $$$$'; ?>
+						  <div class="mx-3">
+						    <div class="md-form mb-5">
+							  
+							  <input type="text" id="socialSecurityNumber" class="form-control validate">
+							  <label data-error="wrong" data-success="right" for="socialSecurityNumber">Numéro sécurité sociale</label>
+							</div>
+							<div class="md-form mb-5">
+								  <input type="text" id="firstname" class="form-control validate">
+								  <label data-error="wrong" data-success="right" for="firstname">Prénom</label>
+								</div>
+								
+							<div class="md-form mb-5">
+								  
+							  <input type="text" id="lastname" class="form-control validate">
+							  <label data-error="wrong" data-success="right" for="lastname">Nom</label>
+							</div>
+								
+							
+							
+							<div class="md-form mb-5">
+								  
+							  <input type="text" id="weight" class="form-control validate">
+							  <label data-error="wrong" data-success="right" for="weight">Poids</label>
+							</div>
+
+
+						  </div>
+						  <button type="submit" class="btn btn-default">Enregistrer</button>
+						</form>
+				</div>
+				
+			
+			</div>
+
+          </div>
+          <!--Grid column-->
+
+        </div>
+        <!--Grid row-->
+
+      </section>
+      <!--Section: Main info-->
+
+
+    </div>
+  </main>
+  <!--Main layout-->
+
+ <?php require ('footer.php'); ?>
+</body>
+
+</html>
---
+++ medictionary-front/user-menu.php
@@ -0,0 +1,22 @@
+
+<?php if (basename($_SERVER['PHP_SELF']) == 'compte.php') { ?>
+	<button id="allergies-page" class="btn btn-warning btn-sm " onclick="location.href = '/compte';">Compte</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/edit-user';">Profil</button>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/allergies';">Allergies</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/commandes';">Commandes</button>
+<?php }else if (basename($_SERVER['PHP_SELF']) == 'edit-user.php') { ?>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/compte';">Compte</button>
+	<button class="btn btn-warning btn-sm " onclick="location.href = '/edit-user';">Profil</button>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/allergies';">Allergies</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/commandes';">Commandes</button>
+<?php }else if (basename($_SERVER['PHP_SELF']) == 'allergies.php') { ?>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/compte';">Compte</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/edit-user';">Profil</button>
+	<button id="allergies-page" class="btn btn-warning btn-sm " onclick="location.href = '/allergies';">Allergies</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/commandes';">Commandes</button>
+<?php }else if (basename($_SERVER['PHP_SELF']) == 'commandes.php') { ?>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/compte';">Compte</button>
+	<button class="btn btn-light btn-sm " onclick="location.href = '/edit-user';">Profil</button>
+	<button id="allergies-page" class="btn btn-light btn-sm " onclick="location.href = '/allergies';">Allergies</button>
+	<button class="btn btn-warning btn-sm " onclick="location.href = '/commandes';">Commandes</button>
+<?php } ?>
